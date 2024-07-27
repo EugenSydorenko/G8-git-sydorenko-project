5408f130ef32cdad929397ebae925ab8b78276ce
EugenSydorenko <eugensydorenko@gmail.com>
2024-07-27 15:32:06 +0300

    Job update 3

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     e2e: {
         viewportWidth: 1920,
         viewportHeight: 1080,
-        baseUrl: 'https://brand-usa-dev.netlify.app',
+        baseUrl: 'https://usadiscoveryprogram.com',
         watchForFileChanges: false,
         setupNodeEvents(on, config) {
             allureCypress(on);
